2b70a7912da29ddbd04867464367949981b12b25
Sergey <heyyyoyy@gmail.com>
2023-01-17 23:09:12 +0300

    nvim base config

---
+++ nvim/global.lua
@@ -0,0 +1,16 @@
+local opt = vim.opt
+local g = vim.g
+
+opt.number = true
+opt.relativenumber = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.showmatch = true
+
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.smartindent = true
+opt.splitright = true
+opt.splitbelow = true
+opt.clipboard = 'unnamedplus'
+opt.fixeol = false
